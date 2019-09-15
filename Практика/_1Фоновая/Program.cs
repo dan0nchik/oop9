73d8c8d84ad855e02c497cd8473001b62b865a3e
Daniel Khromov <danielkhromov@gmail.com>
2019-09-15 17:14:28 +0300

    Double вместо float

--- Практика/_1Фоновая/Program.cs
+++ Практика/_1Фоновая/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _1Фоновая
 {
@@ -10,13 +6,18 @@ namespace _1Фоновая
     {
         static void Main(string[] args)
         {
-            float a = float.Parse(Console.ReadLine()), b = float.Parse(Console.ReadLine()), c = float.Parse(Console.ReadLine());
-
-            float result = Math.Pow((c + b), 1 / 3); //+ Math.Sin(a/4) + Math.Sqrt((a + b + 4) / 2) + Math.Pow(c,2);
-            
-            Console.Write(result);
-            Console.ReadKey();
-
+             Console.Write("a:");
+             int a = int.Parse(Console.ReadLine());
+             double a1 = (double)a;
+             Console.Write("b:");
+             int b = int.Parse(Console.ReadLine());
+             double b1 = (double)b;
+             Console.Write("c:");
+             int c = int.Parse(Console.ReadLine());
+             double c1 = (double)c;
+            double result = Math.Pow((c + b), 1 / 3) + Math.Sin(a / 4) + Math.Sqrt((a + b + 4) / 2) + Math.Pow(c, 2);
+             Console.WriteLine("{0:F2}", result);
+             Console.ReadKey(); 
         }
     }
 }
