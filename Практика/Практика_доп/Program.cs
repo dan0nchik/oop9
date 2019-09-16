7715f9e645c31bde63bc7aa10d25de24ba58e286
Daniel Khromov <danielkhromov@gmail.com>
2019-09-17 00:15:24 +0300

    Stable build

---
+++ Практика/Практика_доп/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Практика_доп
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine()), y = int.Parse(Console.ReadLine()), z = int.Parse(Console.ReadLine());
+            Console.WriteLine(x == y && x == z && y == z ? "Yes" : "No");
+            Console.ReadKey();
+        }
+    }
+}
