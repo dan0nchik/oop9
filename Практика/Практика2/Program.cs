509269e21fc5a0ca6a1dedb99e84fb55463be137
Daniel Khromov <danielkhromov@gmail.com>
2019-09-16 10:50:23 +0300

    Final version

--- Практика/Практика2/Program.cs
+++ Практика/Практика2/Program.cs
@@ -10,6 +10,13 @@ namespace Практика2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(int.Parse(Console.ReadLine()) <= 9 ? "Yes" : "No");
+            Console.ReadKey();
+
+           
+
+
+            
         }
     }
 }
