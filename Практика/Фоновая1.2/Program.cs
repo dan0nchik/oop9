a080c7d99970c47766e7b9a4b30de1208b6ed427
Daniel Khromov <danielkhromov@gmail.com>
2019-09-17 10:41:33 +0300

    ver 1.0

---
+++ Практика/Фоновая1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фоновая1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7
+            int x, y;
+
+        }
+    }
+}
---
+++ Фоновые задачи/Фоновая1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фоновая1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7
+            int x = int.Parse(Console.ReadLine()), y = int.Parse(Console.ReadLine()), U;
+            if(x <= 0)
+            {
+
+            }
+
+
+        }
+    }
+}
