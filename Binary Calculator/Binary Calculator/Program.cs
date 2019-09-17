fd21b16e3a8978c307edc1378ea775f5bff0c6c5
Daniel Khromov <danielkhromov@gmail.com>
2019-09-17 22:49:55 +0300

    Binary Calculator 1.0

---
+++ Binary Calculator/Binary Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            string s = "";
+            while(x != 2)
+            {
+                x = x / 2;
+                if(x % 2 == 0)
+                {
+                    s += "0";
+                    Console.WriteLine(s);
+                }
+                else
+                {
+                    s += "1";
+                    x -= 1;
+                    Console.WriteLine(s);
+
+                }
+                s += "1";
+                Console.WriteLine(s);
+           
+            }
+            char[] k = s.ToCharArray();
+            Array.Reverse(k);
+            Console.WriteLine("");
+            Console.WriteLine(k);
+
+            Console.ReadKey();
+        }
+    }
+}
