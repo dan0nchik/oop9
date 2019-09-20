74932a7b1e2f036c2f84581dbb2493d860db0073
Хромов Даниил <hromov.daniil@lit.msu.ru>
2019-09-20 17:44:13 +0300

    All files are in same solution

--- Фоновые задачи/Фоновая1.1/Program.cs
+++ Фоновые задачи/Фоновая1.1/Program.cs
@@ -1,23 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Фоновая1._1
+namespace _1Фоновая
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //7
-            int x = int.Parse(Console.ReadLine()), y = int.Parse(Console.ReadLine()), U;
-            if(x <= 0)
-            {
-
-            }
-
-
+             Console.Write("a:");
+             int a = int.Parse(Console.ReadLine());
+             double a1 = (double)a;
+             Console.Write("b:");
+             int b = int.Parse(Console.ReadLine());
+             double b1 = (double)b;
+             Console.Write("c:");
+             int c = int.Parse(Console.ReadLine());
+             double c1 = (double)c;
+            double result = Math.Pow((c + b), 1.0 / 3) + Math.Sin(a / 4) + Math.Sqrt((a + b + 4.0) / 2) + Math.Pow(c, 2);
+             Console.WriteLine("{0:F}", result);
+             Console.ReadKey(); 
         }
     }
 }
