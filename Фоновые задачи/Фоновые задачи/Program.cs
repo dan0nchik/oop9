1299934c673267e48eb8213df62ff4acb1ff5f62
Daniel Khromov <danielkhromov@gmail.com>
2019-09-21 21:18:57 +0300

    Create Фоновая 1.1

---
+++ Фоновые задачи/Фоновые задачи/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фоновые_задачи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a:");
+            int a = int.Parse(Console.ReadLine());
+            double a1 = (double)a;
+            Console.Write("b:");
+            int b = int.Parse(Console.ReadLine());
+            double b1 = (double)b;
+            Console.Write("c:");
+            int c = int.Parse(Console.ReadLine());
+            double c1 = (double)c;
+            double result = Math.Pow((c + b), 1.0 / 3) + Math.Sin(a / 4) + Math.Sqrt((a + b + 4.0) / 2) + Math.Pow(c, 2);
+            Console.WriteLine("{0:F}", result);
+            Console.ReadKey();
+        }
+    }
+}
