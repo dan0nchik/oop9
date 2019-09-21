ce21643ee2193738ca1c6801fdab5ab634947b8d
Daniel Khromov <danielkhromov@gmail.com>
2019-09-21 20:12:01 +0300

    Фоновая 1.2 version 1.0

--- Фоновые задачи/Фоновая1.1/Program.cs
+++ Фоновые задачи/Фоновая1.1/Program.cs
@@ -11,13 +11,43 @@ namespace Фоновая1._1
         static void Main(string[] args)
         {
             //7
-            int x = int.Parse(Console.ReadLine()), y = int.Parse(Console.ReadLine()), U;
-            if(x <= 0)
-            {
+            double x = double.Parse(Console.ReadLine()), y = double.Parse(Console.ReadLine());
+            double U = 0.0;
+            //проверка левой четверти
+            if (x <= 0.0)
+                if (x >= -1.0)
+                    if (y >= 0.0)
+                        if (y <= 1.0)
+                            // U = Math.Sqrt(Math.Abs(2.0 * y + x));
+                            Console.WriteLine("Yes");
 
-            }
+
+                        else
+
+                            //U = x / Math.Pow(y, 2.0);
+                            Console.WriteLine("No");
+          
 
 
+            //проверка правой четверти с вырезом
+            if (x >= 0.0)
+     
+                if (x <= 1)
+                    if (y >= 0.3)
+                        if (y <= 1.0)
+                            //U = Math.Sqrt(Math.Abs(2.0 * y + x));
+                Console.WriteLine("Yes");
+         
+
+            else
+            
+                // U = x / Math.Pow(y, 2.0);
+                Console.WriteLine("No");
+            
+
+            Console.WriteLine("{0:G3}", U);
+
+            Console.ReadKey();
         }
     }
 }
