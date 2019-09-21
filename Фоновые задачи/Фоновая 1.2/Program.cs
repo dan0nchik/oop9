ccd51856898ddaefa6f05a522ee20812088a3954
Daniel Khromov <danielkhromov@gmail.com>
2019-09-22 00:43:42 +0300

    1.2 (with bug)

---
+++ Фоновые задачи/Фоновая 1.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Фоновая_1._2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // вариант 7
+            double x = double.Parse(Console.ReadLine()), y = double.Parse(Console.ReadLine());
+            double U = 0.0;
+            //проверка на попадание в D
+            //1.08627804912002
+            if (y * y + x * x <= 1.0)
+            {
+                if (x <= 0.0)
+
+                    if (x >= -1.0)
+
+                        if (y >= 0.0)
+
+                            if (y <= 1.0)
+                                U = Math.Sqrt(Math.Abs(2.0 * y + x));
+                            else U = x / Math.Pow(y, 2.0);
+
+                if (x >= 0.0)
+                    if (x >= 0.3)
+                        if (x <= 1.0)
+                            if (y >= 0.3)
+                                if (y <= 1.0)
+                                    U = Math.Sqrt(Math.Abs(2.0 * y + x));
+                                else U = x / Math.Pow(y, 2.0);
+            }
+
+            else U = x / Math.Pow(y, 2.0);
+
+            Console.WriteLine(U);
+            Console.ReadKey();
+        }
+    }
+}
--- Фоновые задачи/Фоновые задачи/Program.cs
+++ Фоновые задачи/Фоновые задачи/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Фоновые_задачи
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.Write("a:");
             int a = int.Parse(Console.ReadLine());
