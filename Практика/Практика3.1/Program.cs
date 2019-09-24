58a73449d994743fecea4f64f7547cab71476cfc
Daniel Khromov <danielkhromov@gmail.com>
2019-09-24 10:40:02 +0300

    3.1 без норм вывода

---
+++ Практика/Практика3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int a = 2; a <= 9; a++)
+            {
+                for(int b = 2; b <= 9; b++)
+                {
+                    int c = a * b;
+                    Console.WriteLine("{0} \t", c);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
