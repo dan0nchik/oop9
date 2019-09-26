02cbdefc97ab37ead8b47ea8f71f73db25401ab1
Daniel Khromov <danielkhromov@gmail.com>
2019-09-26 23:52:55 +0300

    practice 3

--- Практика/Практика3.1/Program.cs
+++ Практика/Практика3.1/Program.cs
@@ -1,24 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Практика3._1
+namespace Практика3
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void PifagorTable()
         {
-            
-            for(int a = 2; a <= 9; a++)
+            Console.Write("\t");
+            for (int n = 2; n <= 9; n++)
             {
-                for(int b = 2; b <= 9; b++)
-                {
-                    int c = a * b;
-                    Console.WriteLine("{0} \t", c);
-                }
+                Console.Write($"{n}\t");
             }
+            Console.WriteLine("\n\n");
+            for (int i = 2; i <= 9; i++)
+            {
+                Console.Write($"{i} \t");
+                for (int j = 2; j <= 9; j++)
+                {
+                    int k = i * j;
+                    Console.Write($"{k}\t");
+                }
+
+                Console.WriteLine($"\n");
+            }
+        }
+
+        private static void _2()
+        {
+            for (int a = 1; a <= 7; a++)
+            {
+                for (int b = 2; b <= 8; b++)
+                {
+                    for (int c = 3; c <= 9; c++)
+                    {
+                        Console.WriteLine($"{a}{b}{c}");
+                        
+                    }
+                    
+                }
+                /*тут должны увеличиваться a и b на 1, но 
+                он не дает их увеличить потому что они относятся к for'ам, 
+                возможно это ограничение c#. Я пробовал с while в for, и просто несколько while, 
+                но тогда он выводит только с 123 по 129*/
+               
+            }
+
+            }
+
+        private static void _3()
+        {
+           
+
+        }
+
+        private static void Main()
+        {
+            //PifagorTable();
+            _2();
             Console.ReadKey();
         }
     }
