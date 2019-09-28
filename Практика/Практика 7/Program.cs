9009dd2d97e5743a2ecf660e4250202db79b5285
Daniel Khromov <danielkhromov@gmail.com>
2019-09-28 23:05:17 +0300

    Practise 4 1.1

--- Практика/Практика 7/Program.cs
+++ Практика/Практика 7/Program.cs
@@ -4,7 +4,7 @@ namespace Практика_4
 {
     internal class Program
     {
-        static double Min(double x, double y, double z)
+         static double Min(double x, double y, double z)
         {
             double min;
             min = x;
@@ -20,7 +20,7 @@ namespace Практика_4
             return min;
         }
 
-        static double Max(double x, double y)
+         static double Max(double x, double y)
         {
             double max;
             max = x;
@@ -34,14 +34,11 @@ namespace Практика_4
         }
 
         private static void Main(string[] args)
-        { 
-
+        {  
             double a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine());
-
-            //Max(a, b);
             double resultFromMin = Min(a, b + 1.0, Math.Abs(a) + 1.0);
-            double result = Max(Math.Sqrt(resultFromMin), Max(a, b + Min(a, b, 0.0))) - Min(Math.Sqrt(a+1.0), (b+5.0)/(2.0+a*a),a+b);
-            Console.WriteLine(result);
+            double result = Max(Math.Sqrt(resultFromMin), Max(a, b + Min(a, b, 0.0))) - Min(Math.Sqrt(a + 1.0), (b + 5.0) / (2.0 + a * a), a + b);
+            Console.WriteLine("{0:G2}", result); 
             Console.ReadKey();
         }
     }
