801111f6992d81f92792a61d64a18646ef24e624
Daniel Khromov <danielkhromov@gmail.com>
2019-10-04 23:50:13 +0300

    Таблица истинности и практика 5

---
+++ Практика/Таблица/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Таблица_истинности
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine(" A \t B \t A & B\t A | B \n false \t false \t false \t false \n true \t false \t false \t true \n false \t true \t false \t true \n true \t true \t true \t true");
+            Console.ReadKey();
+        }
+    }
+}
