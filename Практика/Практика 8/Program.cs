1f0cb248f60a2273ce22d4f372da15926874844f
Daniel Khromov <danielkhromov@gmail.com>
2019-11-28 00:33:22 +0300

    Практика 8 и Фоновая 2.3

---
+++ Практика/Практика 8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_8
+{   
+    class Program
+    {   
+        public static void SumAndDiff(ref int a, ref int b)
+    {
+            int temp = a - b;
+            int t = a + b;
+            a = t;
+            b = temp;
+    }
+        
+        public static void Change(ref int n, int m)
+        {
+            int n1 = n, desyatki = 1, num = 0, n2 = 0;
+            while(n1 > 0)
+            {
+                n1 /= 10;
+                num++;
+                desyatki *= 10;
+            }
+            while(desyatki > 1) { 
+                desyatki /= 10;
+                if(((n / desyatki) % 10) > m) n2 += 9 * desyatki;
+                else
+                if (((n / desyatki) % 10) % m == 0) n2 += m * desyatki;
+                if ((((n / desyatki) % 10) < m) && (((n / desyatki) % 10) % m != 0)) n2 += desyatki * ((n / desyatki) % 10);
+            }
+            n = n2;
+        }
+        
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine());
+            SumAndDiff(ref a, ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            int n = int.Parse(Console.ReadLine()), m = int.Parse(Console.ReadLine());
+            Change(ref n, m);
+            Console.WriteLine(n);
+            Console.ReadKey();
+
+        }
+       
+
+    }
+}
---
+++ Фоновые задачи/Фоновая 2.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фоновая_2._3
+{   
+        
+    
+    class Program
+    {
+        
+        static void Overloaded(ref int n)
+        {   int sum = 0, i = 1;
+            for(; i < n; i++)
+            {
+                if (n % i == 0) sum += i;
+            }
+            if (sum > n) Console.Write("Число {0} является избыточным.", n);
+            else
+            {
+                sum = 0; //сброс суммы
+               while(true)
+                {
+                    n++;
+                    for (int j = 1; j < n; j++)
+                    {
+                        if (n % j == 0) sum += j;
+                    }
+                    if (sum > n) break;
+                }
+                Console.Write("Число не избыточное, ближайшее избыточое - {0}", n);
+            }
+        }
+        static void Main(string[] args)
+        {   
+            Console.OutputEncoding = Encoding.Unicode;
+            int n = int.Parse(Console.ReadLine());
+            Overloaded(ref n);
+            
+            Console.ReadKey();
+        }
+    }
+}
