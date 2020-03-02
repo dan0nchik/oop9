13a82860652f23f0159d760a7f8a82b4d880e883
Daniel Khromov <danielkhromov@gmail.com>
2020-03-02 23:56:08 +0300

    фоновая 4 без золотого сечения и интерфейса

--- Фоновая4/Фоновая4/Program.cs
+++ Фоновая4/Фоновая4/Program.cs
@@ -17,61 +17,133 @@ namespace Фоновая4
                 h = 5;
                 l = 7;
             }
-            public TheIlluminati(double h, double l)
+            /*public TheIlluminati(double h, double l)
             {
                 this.h = h;
                 this.l = l;
-            }
+            }*/
             public void Output()
             {
                 Console.WriteLine($"Высота пирамиды: {h} см");
                 Console.WriteLine($"Основание пирамиды: {l} см");
             }
-            public double Perimeter()
+            public double Perimeter
             {
-                return 4 * l;
+                get
+                {
+                    return 4 * l;
+                }
             }
-            public double Size()
+            public double Size
             {
-                return (h * Math.Pow(l, 2)) / 3;
+                get
+                {
+                    return (h * Math.Pow(l, 2)) / 3;
+                }
             }
-            public double GetHeight()
+            public double GetHeight
             {
-                return h;
+                get
+                {
+                    return h;
+                }
+                set
+                {
+                    try
+                    {
+                        if (value < 0) throw new Exception("Основание меньше 0!");
+                        h = value;
+                    }
+                    catch(Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+                }
             }
-            public double GetGround()
+            public double GetGround
             {
-                return l;
+                get
+                {
+                    return l;
+                }
+                set
+                {
+                    try
+                    {
+                        if (value < 0) throw new Exception("Основание меньше 0!");
+                        l = value;
+                    }
+                    catch(Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+                }
+            }
+            public double SquareOFGround
+            {
+                get
+                {
+                   return l * l;
+                }
+            }
+            public double Apofem
+            {
+                get
+                {
+                    return Math.Sqrt(h * h + Math.Pow(l / (2 * Math.Tan(45.0)),2));
+                }
+            }
+            public double SqrtOfWall
+            {
+                get
+                {
+                    return (Perimeter / 2) * Apofem;
+                }
+            }
+            public double GroundDiagonal
+            {
+                get
+                {
+                    return GetGround * Math.Sqrt(2);
+                }
+            }
+            public double Rib
+            {
+                get
+                {
+                    return Math.Sqrt(h * h + Math.Pow(l / (2 * Math.Sin(45.0)), 2));
+                }
+            }
+            public double PyramidSquare
+            {
+                get
+                {
+                    return SqrtOfWall + SquareOFGround;
+                }
             }
         }
         
         
         static void Main(string[] args)
         {
-            TheIlluminati pyramid;
-                
-              
-            double hight, ground;
+            TheIlluminati pyramid = new TheIlluminati();
             Console.WriteLine("Создать объект со своими значениями - введите 1, или нажмите 2");
-
             int input = int.Parse(Console.ReadLine());
             if (input == 1)
-            {
+            {   
                 Console.Write("Введите высоту: ");
-                hight = double.Parse(Console.ReadLine());
+                pyramid.GetHeight = double.Parse(Console.ReadLine());
                 Console.Write("Введите основание: ");
-                ground = double.Parse(Console.ReadLine());
-                pyramid = new TheIlluminati(hight, ground);
+                pyramid.GetGround = double.Parse(Console.ReadLine());
+                
             }
-            else
-            {
-                pyramid = new TheIlluminati();
-            }
-                Console.WriteLine($"Высота пирамиды: {pyramid.GetHeight()} см");
-                Console.WriteLine($"Основание пирамиды: {pyramid.GetGround()} см");
-                Console.WriteLine($"Периметр пирамиды: {pyramid.Perimeter()} см");
-                Console.WriteLine($"Объём пирамиды: {pyramid.Size()} см в квадрате");
-                Console.ReadKey();
+                Console.WriteLine($"Высота пирамиды: {pyramid.GetHeight} см");
+                Console.WriteLine($"Основание пирамиды: {pyramid.GetGround} см");
+                Console.WriteLine($"Периметр пирамиды: {pyramid.Perimeter} см");
+                Console.WriteLine($"Объём пирамиды: {pyramid.Size} см в квадрате");
+                Console.WriteLine($"Площадь основания пирамиды: {pyramid.SquareOFGround} см в квадрате");
+
+            Console.ReadKey();
         }
     }
 }
