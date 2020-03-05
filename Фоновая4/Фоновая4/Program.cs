dc925ca2c694f3349b2aa01cc8c9c9b34da2f247
Daniel Khromov <danielkhromov@gmail.com>
2020-03-05 23:27:37 +0300

    исправленная фоновая_4

--- Фоновая4/Фоновая4/Program.cs
+++ Фоновая4/Фоновая4/Program.cs
@@ -14,14 +14,14 @@ namespace Фоновая4
             private double l;
             public TheIlluminati()
             {
-                h = 5;
-                l = 7;
+                h = 7;
+                l = 5;
             }
-            /*public TheIlluminati(double h, double l)
+            public TheIlluminati(double h, double l)
             {
                 this.h = h;
                 this.l = l;
-            }*/
+            }
             public void Output()
             {
                 Console.WriteLine($"Высота пирамиды: {h} см");
@@ -90,14 +90,14 @@ namespace Фоновая4
             {
                 get
                 {
-                    return Math.Sqrt(h * h + Math.Pow(l / (2 * Math.Tan(45.0)),2));
+                    return SqrtOfWall / (2 * l);
                 }
             }
             public double SqrtOfWall
             {
                 get
                 {
-                    return (Perimeter / 2) * Apofem;
+                    return (Math.Sqrt( Math.Pow(h,2) + Math.Pow(l/2,2)) * 4 * l) / 2;
                 }
             }
             public double GroundDiagonal
@@ -111,7 +111,7 @@ namespace Фоновая4
             {
                 get
                 {
-                    return Math.Sqrt(h * h + Math.Pow(l / (2 * Math.Sin(45.0)), 2));
+                    return Math.Sqrt(h*h+Math.Pow( GroundDiagonal/2,2));
                 }
             }
             public double PyramidSquare
@@ -121,29 +121,49 @@ namespace Фоновая4
                     return SqrtOfWall + SquareOFGround;
                 }
             }
+            public double GoldenRatio
+            {
+                get
+                {
+                    return 0;
+                }
+            }
         }
         
         
         static void Main(string[] args)
         {
-            TheIlluminati pyramid = new TheIlluminati();
+            TheIlluminati pyramid;
             Console.WriteLine("Создать объект со своими значениями - введите 1, или нажмите 2");
             int input = int.Parse(Console.ReadLine());
             if (input == 1)
-            {   
+            { 
                 Console.Write("Введите высоту: ");
-                pyramid.GetHeight = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
                 Console.Write("Введите основание: ");
-                pyramid.GetGround = double.Parse(Console.ReadLine());
-                
+                double ground = double.Parse(Console.ReadLine());
+                pyramid = new TheIlluminati(height, ground);
             }
-                Console.WriteLine($"Высота пирамиды: {pyramid.GetHeight} см");
-                Console.WriteLine($"Основание пирамиды: {pyramid.GetGround} см");
-                Console.WriteLine($"Периметр пирамиды: {pyramid.Perimeter} см");
-                Console.WriteLine($"Объём пирамиды: {pyramid.Size} см в квадрате");
-                Console.WriteLine($"Площадь основания пирамиды: {pyramid.SquareOFGround} см в квадрате");
+            else pyramid = new TheIlluminati(); 
 
-            Console.ReadKey();
+                Console.WriteLine($"Высота пирамиды: {pyramid.GetHeight:f2} см");
+                Console.WriteLine($"Основание пирамиды: {pyramid.GetGround:f2} см");
+                Console.WriteLine($"Периметр пирамиды: {pyramid.Perimeter:f2} см");
+                Console.WriteLine($"Объём пирамиды: {pyramid.Size:f2} см в квадрате");
+                Console.WriteLine($"Площадь основания пирамиды: {pyramid.SquareOFGround:f2} см в квадрате");
+                Console.WriteLine($"Площадь боковой поверхности: {pyramid.SqrtOfWall:f2} см в квадрате");
+                Console.WriteLine($"Площадь поверхности: {pyramid.PyramidSquare:f2} см в квадрате");
+                Console.WriteLine($"Площадь основания пирамиды: {pyramid.SquareOFGround:f2} см в квадрате");
+                Console.WriteLine($"Диагональ основания: {pyramid.GroundDiagonal:f2} см");
+                Console.WriteLine($"Апофема: {pyramid.Apofem:f2} см");
+                Console.WriteLine($"Ребро: {pyramid.Rib:f2} см");
+                Console.Write($"Изменить высоту: ");
+            pyramid.GetHeight = double.Parse(Console.ReadLine());
+                Console.Write($"Изменить основание: ");
+            pyramid.GetGround = double.Parse(Console.ReadLine());
+            Console.Write($"Новая высота {pyramid.GetHeight} / Новое основание {pyramid.GetGround}");
+
+                Console.ReadKey();
         }
     }
 }
