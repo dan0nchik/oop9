8d5879f347bfb15ce4d2a497da4deda4bc988d47
Daniel Khromov <danielkhromov@gmail.com>
2020-03-13 12:36:29 +0300

    фоновая4.2

--- Фоновые задачи/Фоновая4.1/Фоновая4.1/Program.cs
+++ Фоновые задачи/Фоновая4.1/Фоновая4.1/Program.cs
@@ -6,41 +6,136 @@ using System.Threading.Tasks;
 
 namespace Фоновая4._1
 {
-    public enum Months{
-        Январь = 31,
-        Февраль = 29,
-        Март = 31,
-        Апрель = 30,
-        Май = 31,
-        Июнь =  30,
-        Июль = 31,
-        Август = 31,
-        Сентябрь = 30,
-        Октябрь = 31,
-        Ноябрь = 30,
-        Декабрь = 31
+    
+   
+    class MatrixWeather {
+        static Random rnd = new Random();
+        public enum Months{
+        Январь = 1,
+        Февраль,
+        Март,
+        Апрель,
+        Май,
+        Июнь,
+        Июль ,
+        Август ,
+        Сентябрь ,
+        Октябрь ,
+        Ноябрь ,
+        Декабрь 
     }
-    class MatrixWether {
-        private Months month;
+        public int[] monthAr = {31,29,31,30,31,30,31,31,30,31,30,31}; 
+        private int month;
         private int day;
-        int[,] temperature;
-        public MatrixWether()
+        private int[,] temperature;
+        public MatrixWeather()
         {
-            month = (Months)1;
+            month = 1;
             day = 1;
+            temperature = new int[5, 7];
+            GenTemp(month, temperature);
         }
-        public MatrixWether(int month, int day)
+        public MatrixWeather(int month, int day)
         {
-            this.month = (Months)month;
+            //if (Enum.IsDefined(typeof(Months), month))
+            //{
+            //    _ = ((Months)month).ToString();
+            //}
+            //this.month = (Months)Enum.GetValues(typeof(Months)).GetValue(month-1);
+            this.month = month;
             this.day = day;
             if(day != 1)
             {
                 temperature = new int[5, 7];
             }
+            else
+            {
+                temperature = new int[4, 7];
+            }
+            GenTemp(month, temperature);
         }
-        
-     
-
+        private static int[,] GenTemp(int month, int[,] diary)
+        {
+            for(int i = 0; i < diary.GetLength(0); i++)
+            for (int j = 0; j < diary.GetLength(1); j++)
+            {
+                switch(month){
+                        case 1:
+                            diary[i, j] = rnd.Next(-10, -5);
+                            break;
+                        case 2:
+                            diary[i, j] = rnd.Next(-10, -3);
+                            break;
+                        case 3:
+                            diary[i, j] = rnd.Next(-5, 2);
+                            break;
+                        case 4:
+                            diary[i, j] = rnd.Next(2, 11);
+                            break;
+                        case 5:
+                            diary[i, j] = rnd.Next(8, 19);
+                            break;
+                        case 6:
+                            diary[i, j] = rnd.Next(12, 22);
+                            break;
+                        case 7:
+                            diary[i, j] = rnd.Next(14, 24);
+                            break;
+                        case 8:
+                            diary[i, j] = rnd.Next(12, 22);
+                            break;
+                        case 9:
+                            diary[i, j] = rnd.Next(7, 16);
+                            break;
+                        case 10:
+                            diary[i, j] = rnd.Next(2, 8);
+                            break;
+                        case 11:
+                            diary[i, j] = rnd.Next(-3, 1);
+                            break;
+                        case 12:
+                            diary[i, j] = rnd.Next(-7, -3);
+                            break;
+                    }
+            }
+            //foreach (int i in diary)
+            //    Console.Write(i + " ");
+            return diary;
+        }
+        public void Output()
+        {
+           int dayNum = 1;
+            int i1 = 0, j1 = 0, j = day;
+           Console.WriteLine((Months)Enum.GetValues(typeof(Months)).GetValue(month - 1));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Пн \t Вт \t Ср \t Чт \t Пт \t Сб \t Вс \t");
+            Console.ResetColor();
+            while(dayNum < day) //сдвиг
+            {
+                Console.Write("\t ");
+                temperature[i1, j1 + day-1] = temperature[i1, j1];
+                temperature[i1, j1] = -10000;
+                dayNum++;
+                i1++;
+                j1++;
+            }
+            dayNum = 1;
+            for(int i = 0; i <= temperature.GetLength(0); i++)
+            {
+                for (; j < temperature.GetLength(1); j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{dayNum} ");
+                    Console.ResetColor();
+                    Console.Write($"{temperature[i,j]}\t ");
+                    dayNum++; 
+                    
+                }
+                j = 1;
+                Console.WriteLine();
+            }
+            //Console.WriteLine(monthAr[month - 1]);
+        }
 
     }
 
@@ -48,12 +143,14 @@ namespace Фоновая4._1
     {
         static void Main(string[] args)
         {
-            MatrixWether weather;
+            MatrixWeather weather;
+            
             Console.Write("Конструктор по умолчанию - 1/ с вашими данными - 2");
             int input = int.Parse(Console.ReadLine());
             if(input == 1)
             {
-                weather = new MatrixWether();
+                weather = new MatrixWeather();
+                weather.Output();
             }
             if (input == 2)
             {
@@ -61,8 +158,11 @@ namespace Фоновая4._1
                 int month = int.Parse(Console.ReadLine());
                 Console.Write("Введите день: ");
                 int day = int.Parse(Console.ReadLine());
-                weather = new MatrixWether(month, day);
+                weather = new MatrixWeather(month, day);
+                weather.Output();
             }
+            
+            Console.ReadKey();
         }
     }
 }
