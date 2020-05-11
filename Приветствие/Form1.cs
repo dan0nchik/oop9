2603ed343100b58f4b1d3b6545cbc75c25c2edc4
Daniel Khromov <danielkhromov@gmail.com>
2020-05-11 21:59:14 +0300

    приложение "приветствие"

---
+++ Приветствие/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Приветствие
+{
+    public partial class Hello : Form
+    {
+        public Hello()
+        {
+            InitializeComponent();
+        }
+
+        private void Hello_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                textBox2.Text = $"Привет, {textBox1.Text}!";
+            }
+            else
+            {
+                textBox2.Text = "Здравствуй, мир!";
+            }
+        }
+    }
+}
