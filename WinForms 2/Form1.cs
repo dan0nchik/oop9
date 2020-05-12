8fd385e1d9b74b687183983a59da3b9b98ee4f1d
Daniel Khromov <danielkhromov@gmail.com>
2020-05-12 10:28:38 +0300

    winforms практика 2

---
+++ WinForms 2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double miles;
+            if (double.TryParse(textBox1.Text, out miles))
+            {
+                label2.Text = $"Указанное число миль равно: {miles / 0.621371} км";
+            }
+            else
+            {
+                label2.Text = "Это не число!";
+            }
+            
+        }
+
+
+    }
+}
