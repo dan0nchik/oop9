91e62b73fece3a1279016809c3d0ebd752249ea1
Daniel Khromov <danielkhromov@gmail.com>
2020-05-16 13:40:39 +0300

    фоновая 6.1 no design & btn centering

---
+++ фоновая6.1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace фоновая6._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(103, 156);
+            this.A.MinimumSize = new System.Drawing.Size(47, 47);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(232, 103);
+            this.A.TabIndex = 0;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = true;
+            this.A.Click += new System.EventHandler(this.A_Click);
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(447, 156);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(232, 103);
+            this.B.TabIndex = 1;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = true;
+            this.B.Click += new System.EventHandler(this.B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
+            this.MinimumSize = new System.Drawing.Size(826, 521);
+            this.Name = "Form1";
+            this.Text = "Crazy buttons";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button B;
+    }
+}
+
---
+++ фоновая6.1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace фоновая6._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Size defaultASize;
+        public Point defaultBLocation;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            defaultASize = A.Size;
+            defaultBLocation = B.Location;
+        }
+        private void A_Click(object sender, EventArgs e)
+        {
+            int xB = B.Location.X, yB = B.Location.Y;
+            Console.WriteLine(ActiveForm.Size.Width);
+            Console.WriteLine(B.Right);
+            B.Location = new Point(xB+4, yB+2);
+            if(B.Right >= ActiveForm.Size.Width-10 || B.Bottom >= ActiveForm.Size.Height-10)
+            {
+                B.Location = defaultBLocation;
+            }
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            int aW = A.Size.Width, aH = A.Size.Height;
+
+            A.Size = new Size(aW-2, aH);
+            if (A.Size.Width == A.MinimumSize.Width)
+            {
+                A.Size = defaultASize;
+            }
+        }
+    }
+}
